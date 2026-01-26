36f696b55759baac2cd79f842801a3c92394d5dc
Migs <41942945+amarabejo0308@users.noreply.github.com>
2026-01-26 20:23:33 +0800

    Add Task model

---
+++ src/model/Task.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Task {
+  private int id;
+  private String title;
+  private String status;
+
+  public Task(int id, String title, String status){
+    this.id = id;
+    this.title = title;
+    this.status = status;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public String getTitle(){
+    return title;
+  }
+
+  public String getStatus(){
+    return status;
+  }
+
+  public void setStatus(String status){
+    this.status = status;
+  }
+}
